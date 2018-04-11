f96862a82b4d51a71716320f6ba12a8c2885867e
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-11 17:42:17 -0400

    [NABI-14] added ImageUploader and integrated it with ProfileBuilder

--- src/ts/ProfileBuilder/components/BasicInfo/BasicInfo.tsx
+++ src/ts/ProfileBuilder/components/BasicInfo/BasicInfo.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import Typography from 'material-ui/Typography';
+import ImageUploader from './ImageUploader';
 
 const BasicInfo = () => {
-    return (
-        // this typography element is temporary
-        // TODO: remove typography
-        <Typography variant="title" className="nabi-margin-top-medium nabi-margin-bottom-medium">
-            Basic Info
-        </Typography>
-    );
+  return (
+    <>
+      {/* This typography element is temporary, TODO: remove typography */}
+      <Typography variant="title" className="nabi-margin-top-medium nabi-margin-bottom-medium">
+          Basic Info
+      </Typography>
+      <ImageUploader />
+    </>
+  );
 };
 
 export default BasicInfo;
---
+++ src/ts/ProfileBuilder/components/BasicInfo/ImageUploader.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Typography from 'material-ui/Typography';
+
+const ImageUploader = () => {
+    return (
+        // this typography element is temporary
+        // TODO: remove typography
+        <Typography variant="body1">
+            Image Uploader
+        </Typography>
+    );
+};
+
+export default ImageUploader;
